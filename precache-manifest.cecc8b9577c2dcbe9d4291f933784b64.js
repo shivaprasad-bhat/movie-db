8e294b701b83fb69399552dd0cda56bea19856fc
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-09-12 12:28:44 +0530

    Updates

---
+++ precache-manifest.cecc8b9577c2dcbe9d4291f933784b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b737f03b696939608a5c3f22f2efe08",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "b95a095787f24f497648",
+    "url": "/movie-db/static/css/main.27be6563.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/movie-db/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie-db/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b95a095787f24f497648",
+    "url": "/movie-db/static/js/main.48d13715.chunk.js"
+  },
+  {
+    "revision": "5faa26bf77a2beb4c5ab",
+    "url": "/movie-db/static/js/runtime-main.694bdeb3.js"
+  }
+]);
